5a45d7a0dd5d3a545667436f01b2b7339543fe0e
lake2 <lake2@qq.com>
2020-04-12 10:41:47 +0800

    rename

--- android/app/src/main/java/cn/lake2/suite/MainActivity.java
+++ android/app/src/main/java/cn/lake2/suite/MainActivity.java
@@ -1,4 +1,4 @@
-package com.suite;
+package cn.lake2.suite;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "suite";
+    return "RNSuite";
   }
 }
